6682b695be7ca9a6886e52b357e0ba5455971e25
Kenny Onishi <kenny619@gmail.com>
2024-09-15 13:31:51 +0900

    init

---
+++ .wrangler/tmp/bundle-BstVCt/middleware-insertion-facade.js
@@ -0,0 +1,10 @@
+				import worker, * as OTHER_EXPORTS from "/Users/kenny/dev/tweet-imgs-cloudflare/src/index.ts";
+				import * as __MIDDLEWARE_0__ from "/Users/kenny/dev/tweet-imgs-cloudflare/node_modules/.pnpm/wrangler@3.78.2_@cloudflare+workers-types@4.20240909.0/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+
+				export * from "/Users/kenny/dev/tweet-imgs-cloudflare/src/index.ts";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default
+				]
+				export default worker;
