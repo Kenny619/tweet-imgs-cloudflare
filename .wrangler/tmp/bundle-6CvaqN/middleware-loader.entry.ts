836fb1a83db2294c1894fc085d96c9f08c9d6a7f
Kenny Onishi <kenny619@gmail.com>
2024-09-15 13:40:00 +0900

    init

--- .wrangler/tmp/bundle-6CvaqN/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-6CvaqN/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/kenny/dev/tweet-imgs-cloudflare/.wrangler/tmp/bundle-BstVCt/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/kenny/dev/tweet-imgs-cloudflare/.wrangler/tmp/bundle-6CvaqN/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/Users/kenny/dev/tweet-imgs-cloudflare/node_modules/.pnpm/wrangler@3.78.2_@cloudflare+workers-types@4.20240909.0/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/kenny/dev/tweet-imgs-cloudflare/.wrangler/tmp/bundle-BstVCt/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/kenny/dev/tweet-imgs-cloudflare/.wrangler/tmp/bundle-6CvaqN/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/kenny/dev/tweet-imgs-cloudflare/.wrangler/tmp/bundle-BstVCt/middleware-insertion-facade.js";
+export * from "/Users/kenny/dev/tweet-imgs-cloudflare/.wrangler/tmp/bundle-6CvaqN/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
